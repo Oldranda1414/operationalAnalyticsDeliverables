d3cb5026646dbb43a57d3def47fdca18685f6cf2
Oldranda <leonardorandacio@gmail.com>
2025-04-10 13:54:50 +0200

    refactoring

--- deliverableEx2.py
+++ deliverableEx2.py
@@ -4,20 +4,24 @@ from statsmodels.tsa.arima.model import ARIMA
 import matplotlib.pyplot as plt
 import pmdarima as pm
 
-def preprocessing(data, m):
-    # Preprocessing, log - diff transform
-    logdata = np.log(train)  # log transform
-    logdiff = np.array([logdata[i] - logdata[i - 1] for i in range(1, len(logdata))])  # logdiff transform
-    logdiffm = np.array([logdiff[i] - logdiff[i - m] for i in range(1, len(logdiff))])  # logdiffm transform
-    return logdata, logdiff, logdiffm
+def preprocessing(data, seasonalm):
+    # log transform
+    log = np.log(data)
+
+    # diff transform
+    logdiff = np.array([log[i] - log[i - 1] for i in range(1, len(log))])
+
+    # diffm transform
+    logdiffm = np.array([logdiff[i] - logdiff[i - seasonalm] for i in range(1, len(logdiff))])
+    return log, logdiff, logdiffm
 
 
-def reconstruct_fore(logdiff, logdata, forecast, m):
+def reconstruct_fore(logdiff, log, forecast, seasonalm):
     # Undo seasonal differencing
-    reclogdiff = [forecast[i] + logdiff[-m + i % m] for i in range(len(forecast))]
+    reclogdiff = [forecast[i] + logdiff[-seasonalm + i % seasonalm] for i in range(len(forecast))]
 
     # Undo regular differencing
-    reclog = [logdata[-1] + reclogdiff[0]] + [0] * (len(reclogdiff) - 1)
+    reclog = [log[-1] + reclogdiff[0]] + [0] * (len(reclogdiff) - 1)
     for i in range(1, len(reclogdiff)):
         reclog[i] = reclog[i - 1] + reclogdiff[i]
 
@@ -31,13 +35,13 @@ train,test = rawdata[:-12], rawdata[-12:]
 
 m = 12
 
-logdata, logdiff, logdiffm = preprocessing(train, m)
+logdata, logdiffdata, logdiffmdata = preprocessing(train, m)
 
 # forecast
 
 # Arma prediction and forecast
 
-model = ARIMA(logdiffm, order=(2, 0, 1))
+model = ARIMA(logdiffmdata, order=(2, 0, 1))
 model_fit = model.fit()
 # make prediction (in-sample or out-of-sample)
 # armaPred = model_fit.predict(0, len(logdiff))
@@ -46,7 +50,7 @@ armaFore = model_fit.forecast(len(test))
 
 # Arima prediction and forecast
 
-model = ARIMA(logdiffm, order=(2, 1, 1))
+model = ARIMA(logdiffmdata, order=(2, 1, 1))
 model_fit = model.fit()
 # make prediction (in-sample or out-of-sample)
 # armaPred = model_fit.predict(0, len(logdiff))
@@ -55,8 +59,8 @@ arimaFore = model_fit.forecast(len(test))
 
 # reconstruct
 
-armaRecFore = reconstruct_fore(logdiff, logdata, armaFore, m)
-arimaRecFore = reconstruct_fore(logdiff, logdata, arimaFore, m)
+armaRecFore = reconstruct_fore(logdiffdata, logdata, armaFore, m)
+arimaRecFore = reconstruct_fore(logdiffdata, logdata, arimaFore, m)
 
 # Sarima model
 model = pm.auto_arima(train, start_p=1, start_q=1,
@@ -82,6 +86,6 @@ plt.plot(range(len(rawdata)-12,len(rawdata)),armaRecFore,label="arma prediction"
 plt.plot(range(len(rawdata)-12,len(rawdata)),arimaRecFore,label="arima prediction")
 plt.plot(range(len(train), len(train) + len(sarimaFore)), sarimaFore,label="sarima prediction")
 plt.plot(range(len(rawdata)-12,len(rawdata)),rawdata[-12:], label="test")
-plt.title("M3 series"),plt.xlabel("time"),plt.ylabel("value"),plt.ylim(0,10000)
+plt.title("M3 series"),plt.xlabel("time"),plt.ylabel("value")
 plt.legend()
 plt.show()
